e66d5af68ca50a4f97c1e05741f751d39e751b68
hemanth kumar <hpeddameti@eniquesolutions.com>
2021-01-08 17:39:15 +0530

    projectAWS added

---
+++ AWSDynamoDBMule/dynamo_db/src/main/java/dynamo_db/DynamoDB.java
@@ -0,0 +1,62 @@
+package dynamo_db;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+
+public class DynamoDB extends com.amazonaws.services.dynamodbv2.document.DynamoDB implements Driver {
+
+	public DynamoDB(AmazonDynamoDB client) {
+		super(client);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Connection connect(String url, Properties info) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean acceptsURL(String url) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMajorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMinorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean jdbcCompliant() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
